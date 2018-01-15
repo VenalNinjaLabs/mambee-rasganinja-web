f6f9b16e3aa2bf3bba177c73b6da802aa6a41568
Kelvin Campelo Alves de Sousa <kelvin.campelo@ifpi.edu.br>
2018-01-15 12:05:49 -0300

    fix name and status bar color

--- src/components/Shared/Toolbar.vue
+++ src/components/Shared/Toolbar.vue
@@ -1,15 +1,15 @@
 <template>
-    <md-toolbar class="md-primary"
-                md-elevation="1">
-        <router-link to="/"
-                     style="flex: 1">
-            <h3 class="md-title">Rasgar Ninja</h3>
-        </router-link>
-        <md-button :to="{name:'lista'}"
-                   class="md-primary button">Ver lista</md-button>
-        <md-button :to="{name:'cadastrar'}"
-                   class="md-raised md-accent button">Rasgar</md-button>
-    </md-toolbar>
+  <md-toolbar class="md-primary"
+              md-elevation="1">
+    <router-link to="/"
+                 style="flex: 1">
+      <h3 class="md-title">Rasga Ninja</h3>
+    </router-link>
+    <md-button :to="{name:'lista'}"
+               class="md-primary button">Ver lista</md-button>
+    <md-button :to="{name:'cadastrar'}"
+               class="md-raised md-accent button">Rasgar</md-button>
+  </md-toolbar>
 </template>
 
 <script>
